4f85c5d4c458aa2aa4445aad93235fa1c14ee629
kazuu <kazunari.tabei@gmail.com>
2021-08-22 13:19:14 +0900

    README追記

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # raspberrypi_dht11_python_web
+ラズパイに接続したdht11から温湿度を取得して、index.htmlを作成(main.py)
+Pythonの簡易httpサーバで公開する(server.py)
+
+## 下準備
+### DHT11のPythonライブラリを取得
+`git clone https://github.com/szazo/DHT11_Python.git`
+* 参考URL：https://qiita.com/mininobu/items/1ba0223af84be153b850
+
+### ライブラリをimportするために、リンクを貼る
+`ln -s DHT11_Python/dht11`
+
+
---
+++ main.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin=14)
+
+# output value a time
+result = instance.read()
+if result.is_valid():
+    msg = u"{0:0.1f}℃ {1:0.1f}％".format(result.temperature, result.humidity)
+else:
+    msg = "err"
+
+# make html
+f = open('index.html', 'w')
+f.write('<!DOCTYPE html><html><meta charset="UTF-8"><body>\n')
+f.write(msg + "\n")
+f.write("</body></html>\n")
+f.close()
+
+# cleanup GPIO
+GPIO.cleanup()
